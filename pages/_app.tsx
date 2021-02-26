e23cffaad41cc5c98ebcf52f29ac3dc6883562ed
tani <tani@1-10.com>
2021-02-26 09:12:06 +0900

    Add recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
+import { AppProps } from 'next/app'
+import { RecoilRoot } from 'recoil'
 
-const Myapp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  )
 }
-export default Myapp
+
+export default MyApp
